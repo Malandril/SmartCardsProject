ee068d7e67663b3e4b2c7b23f3e4b8331890bdd3
Thomas <canava.thomas@gmail.com>
2019-01-17 11:39:30 +0100

    stuff

--- Form1.cs
+++ Form1.cs
@@ -5,7 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
-
+using AskReaderLib;
 namespace CSharpNETTestASKCSCDLL
 {
     public partial class Form1 : Form
@@ -22,7 +22,7 @@ namespace CSharpNETTestASKCSCDLL
 
         private void Button1_Click(object sender, EventArgs e)
         {
-            AskReaderLib.CSC.sCARD_SearchExtTag SearchExtender;
+            CSC.sCARD_SearchExtTag SearchExtender;
             int Status;
             byte[] ATR;
             ATR = new byte[200];
@@ -36,7 +36,7 @@ namespace CSharpNETTestASKCSCDLL
 
             try
             {
-                AskReaderLib.CSC.SearchCSC();
+                CSC.SearchCSC();
                 // user can also use line below to speed up coupler connection
                 //AskReaderLib.CSC.Open ("COM2");
 
@@ -52,12 +52,12 @@ namespace CSharpNETTestASKCSCDLL
                 SearchExtender.MONO = 0;
 
                 // Define type of card to be detected
-                SearchMask = AskReaderLib.CSC.SEARCH_MASK_ISOB | AskReaderLib.CSC.SEARCH_MASK_ISOA;
-                Status = AskReaderLib.CSC.SearchCardExt(ref SearchExtender, SearchMask, 1, 20, ref Com, ref lgATR, ATR);
+                SearchMask = CSC.SEARCH_MASK_ISOB | CSC.SEARCH_MASK_ISOA;
+                Status = CSC.SearchCardExt(ref SearchExtender, SearchMask, 1, 20, ref Com, ref lgATR, ATR);
                 Console.WriteLine("lgATr "+lgATR);
 
                 Console.WriteLine("Status " + Status);
-                if (Status != AskReaderLib.CSC.RCSC_Ok)
+                if (Status != CSC.RCSC_Ok)
                     txtCom.Text =  "Error :" + Status.ToString ("X");
                 else
                     txtCom.Text = Com.ToString("X");
@@ -87,7 +87,7 @@ namespace CSharpNETTestASKCSCDLL
             {
                 MessageBox.Show("Error on trying do deal with reader");
             }
-            AskReaderLib.CSC.Close();
+            CSC.Close();
         }
 
         private void txtCard_TextChanged(object sender, EventArgs e)
@@ -97,7 +97,7 @@ namespace CSharpNETTestASKCSCDLL
 
         private void button2_Click(object sender, EventArgs e)
         {
-            AskReaderLib.CSC.sCARD_SearchExtTag SearchExtender;
+            CSC.sCARD_SearchExtTag SearchExtender;
             int Status;
             byte[] ATR;
             ATR = new byte[200];
@@ -111,7 +111,7 @@ namespace CSharpNETTestASKCSCDLL
 
             try
             {
-                AskReaderLib.CSC.SearchCSC();
+                CSC.SearchCSC();
                 // user can also use line below to speed up coupler connection
                 //AskReaderLib.CSC.Open ("COM2");
 
@@ -125,15 +125,15 @@ namespace CSharpNETTestASKCSCDLL
                 SearchExtender.MV4k = 0;
                 SearchExtender.MV5k = 0;
                 SearchExtender.MONO = 0;
-
+                Status = CSC.CSC_EHP_PARAMS_EXT(1, 1, 0, 0, 0, 0, 0, 0, null, 0, 0);
                 // Define type of card to be detected
-                SearchMask = AskReaderLib.CSC.SEARCH_MASK_ISOB | AskReaderLib.CSC.SEARCH_MASK_ISOA;
-                Status = AskReaderLib.CSC.SearchCardExt(ref SearchExtender, SearchMask, 1, 20, ref Com, ref lgATR, ATR);
+                SearchMask = CSC.SEARCH_MASK_ISOB | CSC.SEARCH_MASK_ISOA;
+                Status = CSC.SearchCardExt(ref SearchExtender, SearchMask, 1, 20, ref Com, ref lgATR, ATR);
                 Console.WriteLine("lgATr " + lgATR);
                 Console.WriteLine("SearchExtender " + SearchExtender.ISOA + " " + SearchExtender.ISOB);
-                Console.WriteLine("ATR", AskReaderLib.CSC.ToStringN(ATR));
+                Console.WriteLine("ATR", CSC.ToStringN(ATR));
                 Console.WriteLine("Status " + Status);
-                if (Status != AskReaderLib.CSC.RCSC_Ok)
+                if (Status != CSC.RCSC_Ok)
                     txtCom.Text = "Error :" + Status.ToString("X");
                 else
                     txtCom.Text = Com.ToString("X");
@@ -158,12 +158,45 @@ namespace CSharpNETTestASKCSCDLL
                 else
                     txtCard.Text = "";
 
-                byte[] selectCommand = new byte[] { 0x00 ,0xA4, 0x04, 0x00 ,0x07, 0xD2, 0x76, 0x00, 0x00, 0X85, 0x01, 0x01, 0x00 };
-                byte[] output=new byte[20];
-                int outSize=0;
-                int returnCode=AskReaderLib.CSC.CSC_ISOCommand(selectCommand, 13, output, ref outSize);
-                Console.WriteLine("Return code "+returnCode);
-                Console.WriteLine(AskReaderLib.CSC.ToStringN(output));
+                byte[] byBuffIn = new byte[] { 0x00, 0xA4, 0x04, 0x00, 0x07, 0xD2, 0x76, 0x00, 0x00, 0X85, 0x01, 0x01, 0x00 };
+                byte[] buffOut = new byte[200];
+                int outSize = 300;
+                int returnCode = CSC.CSC_ISOCommand(byBuffIn, byBuffIn.Length, buffOut, ref outSize);
+                Console.WriteLine(returnCode==CSC.RCSC_Ok);
+                if (returnCode == CSC.RCSC_Ok && outSize > 2 && buffOut[outSize - 2] == 0x90 && buffOut[outSize - 1] == 0x00)
+                {
+                    outSize = 300;
+                    byBuffIn = new byte[] { 0x00, 0xA4, 0x00, 0x0C, 0x02, 0xE1, 0x03 };
+                    returnCode = CSC.CSC_ISOCommand(byBuffIn, buffOut.Length, buffOut, ref outSize);
+                    if (returnCode == CSC.RCSC_Ok && outSize > 2 && buffOut[outSize - 2] == 0x90 && buffOut[outSize - 1] == 0x00)
+                    {
+                        outSize = 300;
+                        byBuffIn = new byte[] { 0x00, 0xB0, 0x00, 0x00, 0x0F };
+                        returnCode = CSC.CSC_ISOCommand(byBuffIn, buffOut.Length, buffOut, ref outSize);
+                        if (returnCode == CSC.RCSC_Ok && outSize > 2 && buffOut[outSize - 2] == 0x90 && buffOut[outSize - 1] == 0x00)
+                        {
+                            int maxLe = buffOut[4] << 8 | buffOut[5];
+                            int maxLc = buffOut[6] << 8 | buffOut[7];
+                            byte[] lid = new byte[] { buffOut[10], buffOut[11] };
+                            int maxLength = buffOut[12] << 8 | buffOut[13];
+                            Console.WriteLine(maxLe + " " + maxLc + " " + lid + " " + maxLength);
+                            outSize = 300;
+                            byBuffIn = new byte[] { 0x00, 0xA4, 0x00, 0x0C, 0x02, lid[0], lid[1] };
+                            returnCode = CSC.CSC_ISOCommand(byBuffIn, buffOut.Length, buffOut, ref outSize);
+                            if (returnCode == CSC.RCSC_Ok && outSize > 2 && buffOut[outSize - 2] == 0x90 && buffOut[outSize - 1] == 0x00)
+                            {
+                                byBuffIn = new byte[] { 0x00, 0xB0, 0x00,  (byte)(maxLe>>8),(byte)maxLe};
+                                returnCode = CSC.CSC_ISOCommand(byBuffIn, buffOut.Length, buffOut, ref outSize);
+                                if (returnCode == CSC.RCSC_Ok && outSize > 2 && buffOut[outSize - 2] == 0x90 && buffOut[outSize - 1] == 0x00)
+                                {
+                                    byBuffIn = new byte[] { 0x00, 0xB0, 0x00, 0x00, 0x0F };
+                                    returnCode = CSC.CSC_ISOCommand(byBuffIn, buffOut.Length, buffOut, ref outSize);
+                                }
+                            }
+                        }
+                    }
+                }
+               
             }
             catch
             {
